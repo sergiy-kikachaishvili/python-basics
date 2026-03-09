4ee70e5ccbbc12e3ba69245a65583d49e64c77ac
Serhii Kikacheishvili <serhii.kikacheishvili@outlook.com>
2026-03-10 00:07:44 +0200

    first python basics - day 1

---
+++ basics.py
@@ -0,0 +1,16 @@
+name = "Serhii"
+age = 22
+city = "Singen"
+
+#виводимо код на екран за допомогою функції print()
+print(name)
+print(age)
+print(city)
+print(f"Hello my name is {name}, i have {age} years and now i live in {city}")
+
+#math calkulations
+
+salary = 10000
+monthly = salary / 12
+
+print(f"My monthly salary:{monthly} CHF")
